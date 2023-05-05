7a083a9454220f57e7bdb43aabc233336883d149
nainabirla <89379807+nainabirla@users.noreply.github.com>
2023-05-05 22:36:44 +0530

    Add files via upload

---
+++ Code clause/Project 1 Unit Converter/script.js
@@ -0,0 +1,125 @@
+const celsiusEl = document.getElementById("celsius");
+const FahrenheitEl = document.getElementById("Fahrenheit");
+const KelvinEl = document.getElementById("Kelvin");
+const currencyfirstEl = document.getElementById("currency-first");
+const currencysecondEl = document.getElementById("currency-second");
+const valuefirstEl = document.getElementById("value-first");
+const valuesecondEl = document.getElementById("value-second");
+let feet    =document.getElementById("feet");
+let meter   =document.getElementById("meter");
+let inch    =document.getElementById("inches");
+let cm      =document.getElementById("cm");
+let yard    =document.getElementById("yards");
+let km      =document.getElementById("km");
+let mile    =document.getElementById("miles");
+
+const exchangerateEl = document.getElementById("exchange-rate");
+
+function computeTemp(event){
+    const currentValue = +event.target.value;
+    switch(event.target.name){
+        case "celsius":
+            KelvinEl.value=(currentValue+273.32).toFixed(2)
+            FahrenheitEl.value=(currentValue*1.8+32).toFixed(2)
+            break;
+        case "Fahrenheit":
+            KelvinEl.value=((currentValue-32)/1.8 + 273.32).toFixed(2)
+            celsiusEl.value=((currentValue - 32)/1.8).toFixed(2)
+            break;
+        case "Kelvin":
+            celsiusEl.value=(currentValue-273.32).toFixed(2)
+            FahrenheitEl.value=((currentValue-273.32)*1.8+32).toFixed(2)
+            break;
+        }
+    }
+updateRate();
+function updateRate(){
+    fetch(`https://v6.exchangerate-api.com/v6/de50af28b6936b227aa23372/latest/${currencyfirstEl.value}`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        const rate= data.conversion_rates[currencysecondEl.value];
+        console.log(rate);
+        exchangerateEl.innerText = `1 ${currencyfirstEl.value} = ${rate + " " + currencysecondEl.value}`;
+        valuesecondEl.value= (valuefirstEl.value*rate).toFixed(2);
+    }
+    )};
+
+
+currencyfirstEl.addEventListener("change", updateRate);
+currencysecondEl.addEventListener("change", updateRate);
+valuefirstEl.addEventListener("input", updateRate);
+
+
+
+function feetToOther(val){
+    meter.value =  val/3.2808; 
+    inch.value =  val*12;   
+    cm.value    =  val/0.032808; 
+    yard.value  =  val*0.33333;  
+    km.value    =  val/3280.8; 
+    mile.value  =  val*0.00018939;       
+}
+function meterToOther(val){
+    feet.value = val*3.2808;
+    inch.value = val*39.370;  
+    cm.value   = val/0.01;
+    yard.value = val*1.0936; 
+    km.value   = val/1000;
+    mile.value = val*0.00062137;
+}
+function inchesToOther(val){
+    feet.value = val*0.083333;
+    meter.value = val/39.370;  
+    cm.value = val/0.39370;
+    yard.value = val*0.027778; 
+    km.value = val/39370;
+    mile.value = val*0.000015783;
+}
+function cmToOther(val){
+    feet.value = val*0.032808;
+    meter.value = val/100;  
+    inch.value = val*0.39370;
+    yard.value = val*0.010936; 
+    km.value = val/100000 ;
+    mile.value = val*0.0000062137;
+}
+function yardsToOther(val){
+    feet.value = val*3;
+    inch.value = val*36;  
+    cm.value = val/0.010936;
+    meter.value = val/1.0936; 
+    km.value = val/1093.6;
+    mile.value = val*0.00056818;
+}
+function kmToOther(val){
+    feet.value = val*3280.8;
+    inch.value = val*39370;  
+    cm.value = val*100000;
+    yard.value = val*1093.6; 
+    meter.value = val*1000;
+    mile.value = val*0.62137;
+}
+function milesToOther(val){
+    feet.value = val*5280;
+    inch.value = val*63360;  
+    cm.value = val/0.0000062137;
+    yard.value = val*1760; 
+    km.value = val/0.62137;
+    meter.value = val/0.00062137;
+
+}
+
+
+
+
+function convertToOthers(convertFrom,value){    
+    switch(convertFrom){
+        case "feet" : feetToOther (parseFloat(value)); break;
+        case "meter": meterToOther(parseFloat(value)); break;
+        case "inch" : inchesToOther(parseFloat(value)); break;
+        case "cm"   : cmToOther(parseFloat(value)); break;
+        case "yard" : yardsToOther (parseFloat(value)); break;
+        case "km"   : kmToOther (parseFloat(value)); break;
+        case "mile" : milesToOther(parseFloat(value)); break;
+    }
+}
